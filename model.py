f050363b90748eeec8637a3f06d2f83e42c7b2ff
Aayush181509 <aayush.181509@ncit.edu.np>
2023-01-09 12:25:52 +0545

    Random Forest model creation

---
+++ model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import joblib
+
+#random seed
+seed=42
+#Read original dataset
+iris_df=pd.read_csv("data/Iris.csv")
+iris_df.sample(frac=1,random_state=seed)
+
+#selecting features and target data
+X=iris_df[["SepalLengthCm","SepalWidthCm","PetalLengthCm","PetalWidthCm"]]
+y=iris_df[["Species"]]
+
+#Split data into train and test sets
+
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size=0.3, random_state=seed,stratify=y
+)
+
+#create an instance of the random forest classifier
+
+clf=RandomForestClassifier(n_estimators=100)
+
+clf.fit(X_train,y_train)
+
+#predict on the test_set
+y_pred=clf.predict(X_test)
+
+#Calculate accuracy
+accuracy=accuracy_score(y_test,y_pred)
+print(f"Accuracy: {accuracy}") #Accuracy=0.91
+
+#save the model to disk
+joblib.dump(clf, "output_models/rf_model.sav")
+
